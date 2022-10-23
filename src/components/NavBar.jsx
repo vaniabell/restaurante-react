43f11c1d42f1cf7c099a8661e1e43b9fab033ee7
vaniabell <108189253+vaniabell@users.noreply.github.com>
2022-10-22 19:22:34 -0500

    links on NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,21 @@
+import {Link,NavLink} from 'react-router-dom';
+
 function NavBar() {
     return (  
         <> 
         <div className="w3-top">
           <div className="w3-row w3-padding w3-black">
             <div className="w3-col s3">
-              <a href="#" className="w3-button w3-block w3-black">HOME</a>
+              <Link className="w3-button w3-block w3-black" to="/home">HOME</Link>
             </div>
             <div className="w3-col s3">
-              <a href="#about" className="w3-button w3-block w3-black">ABOUT</a>
+              <Link className="w3-button w3-block w3-black" to="/about">ABOUT</Link>
             </div>
             <div className="w3-col s3">
-              <a href="#menu" className="w3-button w3-block w3-black">MENU</a>
+              <Link className="w3-button w3-block w3-black"to="/menu">MENU</Link>
             </div>
             <div className="w3-col s3">
-              <a href="#where" className="w3-button w3-block w3-black">WHERE</a>
+              <Link className="w3-button w3-block w3-black" to="/where">WHERE</Link>
             </div>
           </div>
         </div>
